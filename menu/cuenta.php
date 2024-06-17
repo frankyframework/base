80c6a2f0205ffde649ba7caa0120efd7e0c38311
ussiel <ussiel@gmail.com>
2024-06-17 16:54:32 -0600

    link admin

--- menu/cuenta.php
+++ menu/cuenta.php
@@ -3,45 +3,39 @@ global $MySession;
 
 return array(
     array('title'=> "Perfil",
-            'children' =>  array(
-                array(
-                 "permiso" =>   "administrar_mi_usuario",
-                 "url" => $MyRequest->url(FRM_MIS_DATOS),
-                 "etiqueta" => _("Editar mis datos")
-                ),
-                 array(
-                 "permiso" =>   "administrar_mi_contrasena",
-                 "url" => $MyRequest->url(FRM_MY_PASSWORD),
-                 "etiqueta" => _("Cambiar mi contraseña")
-                ),
-                array(
-                     "permiso" =>   "admin_dashboard",
-                     "url" => $MyRequest->url(ADMIN),
-                     "etiqueta" => _("Panel de administrador")
-                ),
-         
-               array(
-                "permiso" =>   "eliminar_mi_perfil",
-                "url" => $MyRequest->url(FRM_ELIMINAR_USER),
-                "etiqueta" => _("Eliminar mi cuenta")
-               )
-        )
-               ),
-               array('title'=> "Configuración",
-               'children' =>  array(
-                        
-                  array(
-                   "permiso" =>   "administrar_devices",
-                   "url" => $MyRequest->url(ADMIN_DEVICES),
-                   "etiqueta" => _("Administrar dispositivos")
-                 )
-               )
-
-               admin_array(
+              'children' =>  array(
+                     array(
+                     "permiso" =>   "administrar_mi_usuario",
+                     "url" => $MyRequest->url(FRM_MIS_DATOS),
+                     "etiqueta" => _("Editar mis datos")
+                     ),
+                     array(
+                     "permiso" =>   "administrar_mi_contrasena",
+                     "url" => $MyRequest->url(FRM_MY_PASSWORD),
+                     "etiqueta" => _("Cambiar mi contraseña")
+                     ),
+                     array(
+                            "permiso" =>   "admin_dashboard",
+                            "url" => $MyRequest->url(ADMIN),
+                            "etiqueta" => _("Panel de administrador")
+                     ),
+              
+                     array(
                      "permiso" =>   "eliminar_mi_perfil",
                      "url" => $MyRequest->url(FRM_ELIMINAR_USER),
                      "etiqueta" => _("Eliminar mi cuenta")
-                    )
+                     )
+              )
+       ),
+       array('title'=> "Configuración",
+       'children' =>  array(
+                        
+                     array(
+                     "permiso" =>   "administrar_devices",
+                     "url" => $MyRequest->url(ADMIN_DEVICES),
+                     "etiqueta" => _("Administrar dispositivos")
+                     )
+               )
        ),
 );
 ?>
