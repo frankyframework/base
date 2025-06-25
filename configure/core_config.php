581b3e09326ae534a04842914d7de67df6518a9e
ussiel <ussiel@gmail.com>
2025-06-25 17:25:28 -0600

    captcha

--- configure/core_config.php
+++ configure/core_config.php
@@ -333,13 +333,13 @@ return array(
                             'validation' => array('required' => true),
                             'data' => getRoles(),
                             'value' => '1'
-                    ),
-                    array('path' => 'base/user/captcha',
-                    'type' => 'select',
-                    'label' => 'Habilitar captcha google',
-                    'validation' => array('required' => true),
-                    'data' => ['1' => 'Si','0' => 'No'],
-                    'value' => '0'
+                    ), 
+                    array('path' => 'base/user/captcha-secret',
+                    'type' => 'text',
+                    'label' => 'Secred Google captcha',
+                    'validation' => array('required' => false),
+                    'value' => ''
+                  ),
 
           )
   ),
